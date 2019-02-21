60394f77d6734320a4a4fceba2257120f97e729c
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2019-02-21 14:31:32 -0500

    Added the put route and method-overide for the html form. These forms don't support put request...gotta override it.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 // MONGOOSE/MODEL CONFIG
@@ -55,7 +57,6 @@ app.get("/blogs/new", function(req, res) {
 });
 
 
-
 // CREATE ROUTE
 app.post("/blogs", function(req, res) {
   // create blog
@@ -82,6 +83,28 @@ app.get("/blogs/:id", function(req, res) {
 });
 
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", { blog: foundBlog });
+    }
+  });
+});
+
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
 
 
 
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Blog Index
+* Setup the Blog App
+* Create Blog model
+* Add INDEX route and template
+
+# Basic Layout
+* Add Header and Foooter
+* Include Semantic UI
+* Add Simple Nav
+
+# Putting the C in CRUD
+* Add NEW route
+* Add NEW template
+* Add CREATE route
+* Add CREATE template
+
+# SHOWtime
+* Add Show route
+* Add Show template
+* Add links to show page
+* Style show template
+
+# Edit/Update
+* Add Edit Route
+* Add Edit Form
+* Add Update Route
+* Add Update Form
+* Add Method-Override
