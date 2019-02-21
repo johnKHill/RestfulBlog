05e4dee5038bddb4bb30ad0c22d33eb2b65861ae
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2019-02-21 13:30:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+var bodyParser = require("body-parser"),
+mongoose       = require("mongoose"),
+express        = require("express"),
+app            = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// test blog just to have as an example
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://t3.ftcdn.net/jpg/01/40/09/88/240_F_140098897_sXYKjCf41dmgLLH5iEBE7x7lnCafRjRA.jpg",
+//   body: "HELLO THIS IS A BLOG POST"
+// })
+
+
+
+
+// =========  RESTFUL ROUTES  ================
+
+app.get("/", function(req, res) {
+  res.render("/blogs")
+})
+
+// INDEX ROUTE
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function (err, blogs) {
+    if (err) {
+      console.log("ERROR!");
+    } else {
+      res.render("index", { blogs: blogs })
+    }
+  });
+});
+
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+  res.render("new");
+});
+
+
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+  // create blog
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if(err) {
+      res.render("new");
+    } else {
+        // then, redirect
+      res.redirect("/blogs");
+    }
+  });
+});
+
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", { blog: foundBlog});
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+app.listen(4000, function() {
+  console.log("SERVER IS RUNNING!");
+});
